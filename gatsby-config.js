05a6a9cf18545ba712a8c4c71e83248ab818c3ad
LookyP <looky135@gmail.com>
2020-12-24 01:17:33 +0000

    try fix 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,25 +13,10 @@ module.exports = {
         path: `${__dirname}/src/blog`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-transition-link`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 590,
-            },
-          },
-        ],
-      },
-    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
